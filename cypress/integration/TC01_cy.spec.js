9c248c72d6b3096a636666df5cb737e8c48302ce
Julia Guimarães <yasminjullia@live.com>
2020-02-07 17:21:52 -0300

    merge02

--- cypress/integration/TC01_cy.spec.js
+++ cypress/integration/TC01_cy.spec.js
@@ -3,7 +3,7 @@ describe('Testcase 01', () => {
         cy.visit('https://www.olx.com.br/')
     })
     it('Procurando um frigobar para meu ap novo', () =>{
-        cy.get(id="q").type('frigobar')
+        cy.get('[id="q"]').type('frigobar')
         cy.get().click()
         cy.get(class{"submitBtn"}).click()
        // cy.get('button[type=submit]').as('submitBtn')
@@ -11,9 +11,9 @@ describe('Testcase 01', () => {
         
 
         
-        cy.get('.gLFyf').type('{enter}')
-        cy.contains('Jéssica Campos – Medium').click({force:true})
-        cy.get(':nth-child(3) > .cr').contains('Engenheira de Controle e Automação')
+       // cy.get('.gLFyf').type('{enter}')
+       // cy.contains('Jéssica Campos – Medium').click({force:true})
+        //cy.get(':nth-child(3) > .cr').contains('Engenheira de Controle e Automação')
     })
 });
 
